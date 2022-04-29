d27a8be7f8c3594cddf45546d1676f641e732a05
Mauricio González <mauriciomunak@gmail.com>
2022-04-29 15:30:37 -0500

    Actualización: Alistar para compilación

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 // Data Environment
-// Poner en false para la instrucción $ electron-packager ./ --platform=win32 --arch=x64
-const modedevelop = true;
+// Poner en false para la instrucción $ electron-packager ./compiled/ --platform=win32 --arch=x64
+const modedevelop = false;
 
 const env = {
   urlpy: modedevelop ? "./py/" : "./resources/app/py/",
