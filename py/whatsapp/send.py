360c44fe2f259c556ed870933fe16c559c88b0b9
Mauricio González <mauriciomunak@gmail.com>
2022-04-29 16:03:19 -0500

    Borra todos los tabs de chrome

--- py/whatsapp/send.py
+++ py/whatsapp/send.py
@@ -1,5 +1,20 @@
 import pywhatkit
 import sys
+import webbrowser
+import pyautogui
+import time
+import os    
+
+# CLose all tabs
+os.system("taskkill /im chrome.exe /f")
+# MacOS
+# chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+# Windows
+chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s'
+# Linux
+# chrome_path = '/usr/bin/google-chrome %s'
+webbrowser.get(chrome_path).open_new('google.com')
+
 if len(sys.argv) >= 4:
     id_grupo = sys.argv[1] if len(sys.argv[1]) > 5 else "Kcti8ujnusV2g0ouqhQTho"
     message = sys.argv[2]
