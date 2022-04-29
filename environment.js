36721a3ab0efe5a91808895727b6926715ce3a31
Mauricio González <mauriciomunak@gmail.com>
2022-04-29 15:33:57 -0500

    Actualización: Alistar paraa compilación script en environment

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
 // Data Environment
-// Poner en false para la instrucción $ electron-packager ./compiled/ --platform=win32 --arch=x64
+// Poner en false para la instrucción $ electron-packager ./ --platform=win32 --arch=x64
 const modedevelop = false;
 
 const env = {
